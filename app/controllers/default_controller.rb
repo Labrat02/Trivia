a96c9dafdf3a0225ccbffd74018268ab0a2cb426
Collin Yeadon <collin@yeadongroup.com>
2017-12-08 01:01:23 -0500

    redirect to login without authentication error dialog, load trivia data only on index action

--- app/controllers/default_controller.rb
+++ app/controllers/default_controller.rb
@@ -1,14 +1,22 @@
 class DefaultController < ApplicationController
   include DefaultHelper
-  before_action :index
+  before_action :set_trivia, only: [:index], if: -> { user_signed_in? }
+  skip_before_action :authenticate_user!
 
   def index
+    return if user_signed_in?
+    redirect_to new_user_session_url
+  end
+
+  def about
+  end
+
+  private
+
+  def set_trivia
     @trivia_questions = trivia_questions_preview
     @trivia_questions_count = trivia_questions_count
     @my_questions = my_questions_recent
     @my_questions_count = my_questions_count
-  end
-
-  def about
   end
 end
